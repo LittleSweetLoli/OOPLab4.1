1dafd19d91028c0c344beabb862695a75d5ca9bf
LittleSweetEvil <40055618+LittleSweetLoli@users.noreply.github.com>
2023-05-11 20:34:55 +0500

    Добавьте файлы проекта.

---
+++ OOPLab4.1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace OOPLab4._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CtrlBt = new System.Windows.Forms.CheckBox();
+            this.AllBt = new System.Windows.Forms.CheckBox();
+            this.CreateBt = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // CtrlBt
+            // 
+            this.CtrlBt.AutoSize = true;
+            this.CtrlBt.Location = new System.Drawing.Point(13, 418);
+            this.CtrlBt.Name = "CtrlBt";
+            this.CtrlBt.Size = new System.Drawing.Size(48, 20);
+            this.CtrlBt.TabIndex = 0;
+            this.CtrlBt.Text = "Ctrl";
+            this.CtrlBt.UseVisualStyleBackColor = true;
+            this.CtrlBt.CheckedChanged += new System.EventHandler(this.CtrlBt_CheckedChanged);
+            this.CtrlBt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.CtrlBt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            // 
+            // AllBt
+            // 
+            this.AllBt.AutoSize = true;
+            this.AllBt.Location = new System.Drawing.Point(67, 418);
+            this.AllBt.Name = "AllBt";
+            this.AllBt.Size = new System.Drawing.Size(44, 20);
+            this.AllBt.TabIndex = 1;
+            this.AllBt.Text = "All";
+            this.AllBt.UseVisualStyleBackColor = true;
+            this.AllBt.CheckedChanged += new System.EventHandler(this.AllBt_CheckedChanged);
+            this.AllBt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.AllBt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            // 
+            // CreateBt
+            // 
+            this.CreateBt.AutoSize = true;
+            this.CreateBt.Location = new System.Drawing.Point(117, 418);
+            this.CreateBt.Name = "CreateBt";
+            this.CreateBt.Size = new System.Drawing.Size(69, 20);
+            this.CreateBt.TabIndex = 2;
+            this.CreateBt.Text = "Create";
+            this.CreateBt.UseVisualStyleBackColor = true;
+            this.CreateBt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.CreateBt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CreateBt);
+            this.Controls.Add(this.AllBt);
+            this.Controls.Add(this.CtrlBt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox CtrlBt;
+        private System.Windows.Forms.CheckBox AllBt;
+        private System.Windows.Forms.CheckBox CreateBt;
+    }
+}
---
+++ OOPLab4.1/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPLab4._1
+{
+    public partial class Form1 : Form
+    {
+        int mouseX, mouseY; // переменные отвечающие за координаты мыши
+        int rad = 20; // диаметр кругов
+        public static List<Ccircle> Circles = new List<Ccircle>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e) // создание кругов при нажатии мыши на форму
+        {
+            if (CreateBt.Checked == true)
+            {
+                Ccircle temp = new Ccircle(mouseX, mouseY, rad);
+                temp.vyd = true;
+
+                if (AllBt.Checked == false)
+                {
+                    check();
+                }
+                else
+                {
+                    check();
+                }
+                Circles.Add(temp);
+                this.Invalidate();
+            }
+            else if (CtrlBt.Checked == true)
+            {
+                foreach (Ccircle ccircle in Circles)
+                {
+                    if ((mouseX - (ccircle.x + Math.Sqrt(rad * rad + rad * rad))) * (mouseX - (ccircle.x + Math.Sqrt(rad * rad + rad * rad))) + (mouseY - (ccircle.y + Math.Sqrt(rad * rad + rad * rad))) * (mouseY - (ccircle.y + Math.Sqrt(rad * rad + rad * rad))) <= ccircle.r * ccircle.r)
+                    {
+                        ccircle.vyd = true;
+                        this.Invalidate();
+                    }
+                }
+            }
+            else if (AllBt.Checked == true)
+            {
+                check();
+                foreach (Ccircle ccircle in Circles)
+                {
+                    if ((mouseX - (ccircle.x + Math.Sqrt(rad * rad + rad * rad))) * (mouseX - (ccircle.x + Math.Sqrt(rad * rad + rad * rad))) + (mouseY - (ccircle.y + Math.Sqrt(rad * rad + rad * rad))) * (mouseY - (ccircle.y + Math.Sqrt(rad * rad + rad * rad))) <= ccircle.r * ccircle.r)
+                    {
+                        ccircle.vyd = true;
+                        this.Invalidate();
+                    }
+                }
+            }
+            else
+            {
+                foreach (Ccircle ccircle in Circles)
+                {
+                    if ((mouseX - (ccircle.x + Math.Sqrt(rad * rad + rad * rad))) * (mouseX - (ccircle.x + Math.Sqrt(rad * rad + rad * rad))) + (mouseY - (ccircle.y + Math.Sqrt(rad * rad + rad * rad))) * (mouseY - (ccircle.y + Math.Sqrt(rad * rad + rad * rad))) <= ccircle.r * ccircle.r)
+                    {
+                        check();
+                        ccircle.vyd = true;
+                        this.Invalidate();
+                    }
+                }
+            }
+        }
+
+        private void check()
+        {
+            foreach (Ccircle c in Circles)
+            {
+                c.vyd = false;
+            }
+        }
+        private void ch()
+        {
+            if (AllBt.Checked == true)
+            {
+                checkAll();
+                this.Invalidate();
+            }
+            else
+            {
+                check();
+                Circles.Last().vyd = true;
+                this.Invalidate();
+            }
+        }
+
+        private void checkAll()
+        {
+            foreach (Ccircle c in Circles)
+            {
+                c.vyd = true;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouseX = e.X; mouseY = e.Y;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!e.Control)
+            {
+                CtrlBt.Checked = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control)
+            {
+                CtrlBt.Checked = true;
+            }
+            if (e.KeyCode == Keys.Delete)
+            {
+                DelEl(sender, e);
+            }
+        }
+
+        void DelEl(object sender, KeyEventArgs e)
+        {
+
+            for (int i = 0; i < Circles.Count;)
+            {
+                if (Circles[i].vyd == true)
+                {
+                    Circles.Remove(Circles[i]);
+                    i = 0;
+                }
+                else
+                    i++;
+            }
+            if (Circles.Count > 0)
+            {
+                ch();
+
+            }
+            this.Invalidate();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Ccircle ccircle in Circles)
+            {
+                Pen pen = new Pen(Color.Aqua);
+                if (ccircle.vyd)
+                {
+                    pen.Color = Color.Blue; // цвет при выделении 
+                }
+                else
+                {
+                    pen.Color = Color.Red; // цвет без выделения
+                }
+                e.Graphics.DrawEllipse(pen, ccircle.x, ccircle.y, ccircle.r * 2, ccircle.r * 2);
+            }
+        }
+
+        private void AllBt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (AllBt.Checked == true)
+            {
+
+            }
+            else
+            {
+                if (Circles.Count > 0)
+                {
+                    check();
+                    Circles.Last().vyd = true;
+                    this.Invalidate();
+                }
+            }
+        }
+
+        private void CtrlBt_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+    public class Ccircle
+    {
+        public int x, y, r;
+        public bool vyd;
+        public Ccircle(int x, int y, int r) { this.x = x; this.y = y; this.r = r; }
+        ~Ccircle() { }
+
+    };
+
+}
